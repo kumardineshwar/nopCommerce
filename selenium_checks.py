cfe6c710bea285d5d18b1e4894dcd657e50937c0
DeeKay <kumardineshwar@gmail.com>
2021-09-06 13:04:57 +0200

    update selenium code

---
+++ selenium_checks.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import pytest
+
+
+target_url = "http://34.88.214.184/install"
+
+print("########## Running the Selenium Script ##########")
+
+@pytest.fixture(scope="session")
+def get_driver():
+    global driver
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
+    yield
+    driver.close()
+
+@pytest.mark.usefixtures("get_driver")
+def test_data():
+    driver.get(target_url)
+    element = driver.find_element_by_class_name("navbar-brand")
+    print("########## Checking for Django Boards on the page ##########")
+    assert element.text == "nopCommerce installation"
+
+@pytest.mark.usefixtures("get_driver")
+def test_body():
+    element = driver.find_element_by_xpath('//*[@id="mainMenu"]/form/a[1]')
+    print("########## Checking for Login Button ##########")
+    assert element.text == "Log in"
+
