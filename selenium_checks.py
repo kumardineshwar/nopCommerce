9eacd49f804a70892e858ee6f89c4674f7969cc5
DeeKay <kumardineshwar@gmail.com>
2021-09-11 14:37:09 +0200

    updated selenium test

--- selenium_checks.py
+++ selenium_checks.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python3
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import pytest
 
 
 target_url = "${{ secrets.EXT_IP }}/install"
+#target_url = "http://34.88.214.184/install"
 
 print("########## Running the Selenium Script ##########")
 
@@ -19,13 +21,13 @@ def get_driver():
 @pytest.mark.usefixtures("get_driver")
 def test_data():
     driver.get(target_url)
-    element = driver.find_element_by_class_name("navbar-brand")
-    print("########## Checking for Django Boards on the page ##########")
-    assert element.text == "nopCommerce installation"
+    element = driver.find_element_by_tag_name("h3")
+    print("########## Checking for AdminEmail field on the page ##########")
+    assert element.text == "Store information"
 
-@pytest.mark.usefixtures("get_driver")
-def test_body():
-    element = driver.find_element_by_xpath('//*[@id="mainMenu"]/form/a[1]')
-    print("########## Checking for Login Button ##########")
-    assert element.text == "Log in"
+#@pytest.mark.usefixtures("get_driver")
+#def test_body():
+#    element = driver.find_element_by_xpath('//*[@id="mainMenu"]/form/a[1]')
+#    print("########## Checking for Login Button ##########")
+#    assert element.text == "Log in"
 
